437201206061e55c75fcedf7b2ecf3d3b67a1f12
zhang <884831651@qq.com>
2018-04-30 22:00:36 +0800

    上传

---
+++ src/main/java/com/wp/weipu/service/IArticleService.java
@@ -0,0 +1,11 @@
+package com.wp.weipu.service;
+
+/**
+ * @author zwp
+ * @version 1.0
+ * @description:
+ * @date 2018/4/30
+ */
+
+public interface IArticleService {
+}
---
+++ src/main/java/com/wp/weipu/service/impl/ArticleServiceImpl.java
@@ -0,0 +1,13 @@
+package com.wp.weipu.service.impl;
+
+import com.wp.weipu.service.IArticleService;
+
+/**
+ * @author zwp
+ * @version 1.0
+ * @description:
+ * @date 2018/4/30
+ */
+
+public class ArticleServiceImpl implements IArticleService{
+}
