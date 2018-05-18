665e06323152704a6003e65000686c4fcf440c88
zhang <884831651@qq.com>
2018-05-18 16:51:37 +0800

    ServletInitializer 据说部署的时候能用到

---
+++ src/main/java/com/wp/weipu/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.wp.weipu;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WeipuApplication.class);
+	}
+
+}
