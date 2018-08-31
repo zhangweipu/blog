6e3639d63f1fc7a1d13d9c27406332492f223d3b
884831651@qq.com <19940528zh>
2018-08-31 22:07:43 +0800

    添加user实现

---
+++ src/main/java/com/wp/weipu/entity/User.java
@@ -0,0 +1,152 @@
+package com.wp.weipu.entity;
+
+import java.util.Date;
+import javax.persistence.*;
+
+public class User {
+    @Id
+    @Column(name = "userID")
+    private Integer userid;
+
+    private String usercode;
+
+    private String username;
+
+    private Integer phone;
+
+    private String email;
+
+    private String passwd;
+
+    private String type;
+
+    private String status;
+
+    private Date createdate;
+
+    /**
+     * @return userID
+     */
+    public Integer getUserid() {
+        return userid;
+    }
+
+    /**
+     * @param userid
+     */
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    /**
+     * @return usercode
+     */
+    public String getUsercode() {
+        return usercode;
+    }
+
+    /**
+     * @param usercode
+     */
+    public void setUsercode(String usercode) {
+        this.usercode = usercode == null ? null : usercode.trim();
+    }
+
+    /**
+     * @return username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username
+     */
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    /**
+     * @return phone
+     */
+    public Integer getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone
+     */
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * @return email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    /**
+     * @return passwd
+     */
+    public String getPasswd() {
+        return passwd;
+    }
+
+    /**
+     * @param passwd
+     */
+    public void setPasswd(String passwd) {
+        this.passwd = passwd == null ? null : passwd.trim();
+    }
+
+    /**
+     * @return type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type
+     */
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+
+    /**
+     * @return status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status
+     */
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    /**
+     * @return createdate
+     */
+    public Date getCreatedate() {
+        return createdate;
+    }
+
+    /**
+     * @param createdate
+     */
+    public void setCreatedate(Date createdate) {
+        this.createdate = createdate;
+    }
+}
---
+++ src/main/java/com/wp/weipu/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.wp.weipu.mapper;
+
+import com.wp.weipu.common.base.BaseMapper;
+import com.wp.weipu.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
