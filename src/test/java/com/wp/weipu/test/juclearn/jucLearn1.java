839f48c6d799b0aa42ae36070b28710c272d135e
884831651@qq.com <19940528zh>
2020-07-18 20:52:21 +0800

    四数之和

--- src/test/java/com/wp/weipu/test/juclearn/jucLearn1.java
+++ src/test/java/com/wp/weipu/test/juclearn/jucLearn1.java
@@ -1,10 +1,13 @@
 package com.wp.weipu.test.juclearn;
 
+import sun.awt.Mutex;
+
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * juc的学习
  */
 public class jucLearn1 {
-    ReentrantLock
+    //不懂这个类的作用是什么
+    //Mutex
 }
